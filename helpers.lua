ef3484ceb613beea1c5f2a162ce2857290f0498d
thelaw51 <joegillard3@gmail.com>
2024-10-06 15:24:49 +0100

    added if statment to bash config to only look at windows path when running windows

--- helpers.lua
+++ helpers.lua
@@ -1,14 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local module = {}
 
-
 local function update_theme(config)
-	config.color_scheme = 'Aura (Gogh)'
-	config.font = wezterm.font 'Geist Mono'
+	config.color_scheme = "Aura (Gogh)"
+	config.font = wezterm.font("Geist Mono")
 	config.window_background_opacity = 0.95
-	config.default_prog = { 'C:/Program Files/Git/bin/bash.exe', '-l' }
-
+	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+		config.default_prog = { "C:/Program Files/Git/bin/bash.exe", "-l" }
+	end
 end
 
 function module.apply_to_config(config)
