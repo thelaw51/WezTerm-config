c9db03bc2a619b0f7f99d2e12323914fc203ed6f
Joe <joe@minotaursoftware.co.uk>
2024-10-07 16:40:48 +0100

    changed font size from 12 to 10

--- helpers.lua
+++ helpers.lua
@@ -5,7 +5,7 @@ local module = {}
 local function update_theme(config)
 	config.color_scheme = "Azu (Gogh)"
 	config.font = wezterm.font("Geist Mono")
-	config.font_size = 12
+	config.font_size = 10
 	config.window_background_opacity = 0.95
 	config.keys = {
 		{
