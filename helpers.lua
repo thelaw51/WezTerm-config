1a19e9fe6b31cb0e4c8615316b758deb0881bafa
Joe <joe@minotaursoftware.co.uk>
2024-10-07 15:43:39 +0100

    change theme

--- helpers.lua
+++ helpers.lua
@@ -3,10 +3,17 @@ local wezterm = require("wezterm")
 local module = {}
 
 local function update_theme(config)
-	config.color_scheme = "Aura (Gogh)"
+	config.color_scheme = "Azu (Gogh)"
 	config.font = wezterm.font("Geist Mono")
 	config.font_size = 12
 	config.window_background_opacity = 0.95
+	config.keys = {
+		{
+			key = "t",
+			mods = "CTRL",
+			action = wezterm.action.SpawnTab("DefaultDomain"),
+		},
+	}
 	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 		config.default_prog = { "C:/Program Files/Git/bin/bash.exe", "-l" }
 	end
