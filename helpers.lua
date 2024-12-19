729df6edae371724493f8b1d61d408529f695c97
Joe <joe@minotaursoftware.co.uk>
2024-12-19 16:25:40 +0000

    added keybind to close tab

--- helpers.lua
+++ helpers.lua
@@ -13,6 +13,11 @@ local function update_theme(config)
 			mods = "CTRL",
 			action = wezterm.action.SpawnTab("DefaultDomain"),
 		},
+		{
+			key = "w",
+			mods = "CTRL",
+			action = wezterm.action.CloseCurrentTab({ confirm = false }),
+		},
 	}
 	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 		config.default_prog = { "C:/Program Files/Git/bin/bash.exe", "-l" }
